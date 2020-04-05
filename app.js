b65887708a556eee7aa0f427f5f0ab3bb45a4f8c
Javad Ghaedi <Ghaedi1993@gmail.com>
2020-04-05 17:08:22 +0430

    adding cors middleware

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require("express"),
   bodyParser = require("body-parser"),
+  cors = require("cors"),
   routes = require("./routes"),
   { PORT, mongoUrl } = require("./config");
 
 //connecting to database
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/pinova", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose
+  .connect("mongodb://localhost/pinova", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((db) => {
+    console.log("connected to db");
+  });
 
 //instanciating our app
 const app = express();
@@ -19,6 +24,7 @@ app.use(express.static("public"));
 app.use(bodyParser.json());
 //parse incoming requests with urlencoded format
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use("/", routes);
 app.listen(PORT, () => {
   console.log(`Server running on Port ${PORT}`);
